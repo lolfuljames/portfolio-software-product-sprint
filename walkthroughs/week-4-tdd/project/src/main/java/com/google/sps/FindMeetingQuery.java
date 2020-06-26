ae78f6bd450aff41ce8c42e6009fe6875fdebb73
James Tan <jamestancheemin98@gmail.com>
2020-06-27 04:39:54 +0800

    Refactor Meeting Query Code Structure
    This commit:
     - Adds method extraction
     - Refactor 'while' loop found during combination of timeslots.

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,106 +15,135 @@
 package com.google.sps;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
-import java.util.Iterator;  
 import java.util.Collection;
 import java.util.Collections;
 
 public final class FindMeetingQuery {
 
+  /** Returns true if {@event} is not attended by any attendees in the meeting request */
   private Boolean isEventIrrelevant(Event event, MeetingRequest meeting) {
     Collection<String> eventAttendees = event.getAttendees();
     Collection<String> requiredAttendees = meeting.getAttendees();
-    for (String eventAttendee: eventAttendees) {
-      if (requiredAttendees.contains(eventAttendee)) return false;
-    }
-    return true;
+    return Collections.disjoint(eventAttendees, requiredAttendees);
   }
 
+  /** Returns true if available duration in {@vacantSlot} is less than requested duration */
   private Boolean isSlotInsufficient(TimeRange vacantSlot, MeetingRequest meeting) {
     return vacantSlot.duration() < meeting.getDuration();
+  }
+
+  /** Returns true if {@requeste} has no attendees */
+  private Boolean hasNoAttendees(MeetingRequest request) {
+    return request.getAttendees().size() == 0;
+  }
+
+  /** Returns true if duration in {@request} is negative or over a day long */
+  private Boolean isDurationInvalid(MeetingRequest request) {
+    return request.getDuration() > TimeRange.END_OF_DAY || request.getDuration() < 0;
+  }
+
+  /** Remove events in {@events} that are not attended by any meeting attendees */
+  private void filterEvents(List<Event> events, MeetingRequest request) {
+    events.removeIf(event -> isEventIrrelevant(event, request));
+  }
+
+  private void sortTimeRangeByStart(List<TimeRange> list) {
+    Collections.sort(list, TimeRange.ORDER_BY_START);
+  }
+
+  /** Removes vacant slots in {@vacantSlots} if available duration is insufficient */
+  private void filterVacantSlots(List<TimeRange> vacantSlots, MeetingRequest request) {
+    vacantSlots.removeIf(timeSlot -> isSlotInsufficient(timeSlot, request));
+  }
+
+  /** Adds start/end-bounding TimeRange objects for {@queue} */
+  private void initializeOccupiedQueue(List<TimeRange> queue) {
+    queue.add(TimeRange.fromStartDuration(TimeRange.START_OF_DAY, 0));
+    queue.add(TimeRange.fromStartDuration(TimeRange.END_OF_DAY + 1, 0));
+  }
+
+  /**
+   * Function that updates an TimeRange object at index {@currentSlotIdx} with an 
+   * updated duration if there are TimeRange objects chaining/within the current object.
+   * 03:00 |    B  06:00 |              03:00          07:00
+   * |      A      |   07:00        ->    |      A      |
+   * @param queue List of TimeRange objects, sorted by start time.
+   * @param currentSlotIdx Index of element to be modified in {@queue}
+   */
+  private void combineTimeRange(List<TimeRange> queue, int currentSlotIdx) {
+    int adjSlotIdx = currentSlotIdx + 1;
+    TimeRange currentSlot = queue.get(currentSlotIdx);
+    TimeRange adjSlot = queue.get(adjSlotIdx);
+
+    // start combining adjacent/containing events with regards to current event
+    while (currentSlot.overlaps(adjSlot)) {
+      int curSlotStart = currentSlot.start();
+      int adjSlotEnd = adjSlot.end();
+      // Combine events if it is a chained event.
+      if (!currentSlot.contains(adjSlotEnd)) {
+        TimeRange combinedSlot = TimeRange.fromStartEnd(
+          curSlotStart, adjSlotEnd, false);
+          queue.set(currentSlotIdx, combinedSlot);
+      }
+      // Get next slot in line if available
+      queue.remove(adjSlotIdx);
+      if(adjSlotIdx == queue.size()) break;
+      else adjSlot = queue.get(adjSlotIdx);
+    }
+  }
+
+
+  /**
+   * Creates a vacant slot based on the provided index of the unavailable timeslot.
+   * 
+   * // |OE1|   <vacantEvent here>   |OE2|
+   * @param occupiedQueue Combined list of TimeRange with TimeRange that is unavailable
+   * @param slotIdx Index of TimeRange Object in front of the VacantSlot in {@occupiedQueue}
+   */
+  private TimeRange createVacantSlot(List<TimeRange> occupiedQueue, int slotIdx) {
+    TimeRange occupiedSlot1 = occupiedQueue.get(slotIdx);
+    TimeRange occupiedSlot2 = occupiedQueue.get(slotIdx + 1);
+    return TimeRange.fromStartEnd(occupiedSlot1.end(), occupiedSlot2.start(), false);
   }
 
   public Collection<TimeRange> query(Collection<Event> eventsSource, MeetingRequest request) {
     List<TimeRange> vacantSlots= new ArrayList<TimeRange>();
     List<TimeRange> occupiedQueue = new ArrayList<TimeRange>();
-    List<Event> events = new ArrayList<Event>();
-    // duplicate events list to support removal of events
-    for (Event event: eventsSource) {
-      events.add(event);
+    // Duplicate events list to support removal of events
+    List<Event> events = new ArrayList<>(eventsSource);
+
+    if (hasNoAttendees(request)) {
+      return Collections.singletonList(TimeRange.WHOLE_DAY);
+    }
+    if (isDurationInvalid(request)) {
+      return Collections.emptyList();
     }
 
-    // handle edge-case entries
-    if (request.getAttendees().size() == 0) {
-      vacantSlots.add(TimeRange.WHOLE_DAY);
-      return vacantSlots;
-    }
-    if (request.getDuration() > TimeRange.END_OF_DAY) {
-      return vacantSlots;
-    }
+    // Filter entries that required attendees are not joining
+    filterEvents(events, request);
 
-    // filter entries
-    events.removeIf(event -> isEventIrrelevant(event, request));
-
-    // Create dummy events for StartOfDay and EndOfDay
-    occupiedQueue.add(TimeRange.fromStartDuration(TimeRange.START_OF_DAY, 0));
-    occupiedQueue.add(TimeRange.fromStartDuration(TimeRange.END_OF_DAY + 1, 0));
+    // Create dummy entries for StartOfDay and EndOfDay
+    initializeOccupiedQueue(occupiedQueue);
 
     // Create entry timeslots for the events
     for (Event event: events) {
       occupiedQueue.add(event.getWhen());
     }
-    
-    // sort by time
-    Collections.sort(occupiedQueue, TimeRange.ORDER_BY_START);
 
-    // combine events to make bigger chunks
-    for (int currentEventIdx = 0;
-         currentEventIdx < occupiedQueue.size();
-        currentEventIdx++) {
-      TimeRange currentSlot = occupiedQueue.get(currentEventIdx);
-      int adjEventIdx = currentEventIdx + 1;
+    sortTimeRangeByStart(occupiedQueue);
 
-      // start combining adjacent/containing events with regards to current event
-      while (adjEventIdx < occupiedQueue.size()) {
-        TimeRange adjSlot = occupiedQueue.get(adjEventIdx);
-
-        // If both events are overlapping, combine and remove adj_event from event list
-        if (currentSlot.overlaps(adjSlot)) {
-          int curSlotStart = currentSlot.start();
-          int adjSlotEnd = adjSlot.end();
-          if (currentSlot.contains(adjSlotEnd)) {
-            occupiedQueue.remove(adjEventIdx);
-          } else {
-            // Combine events
-            TimeRange combinedSlot = TimeRange.fromStartEnd(
-              curSlotStart, adjSlotEnd, false);
-            occupiedQueue.set(currentEventIdx, combinedSlot);
-            occupiedQueue.remove(adjEventIdx);
-          }
-        } else {
-        //  Both events are entirely separate events, stop checking current events for
-        //  overlaps
-          break;
-        }
-      }
+    // Combine unavailable timeslots so that no two timeslots are connecting
+    for (int i = 0; i < occupiedQueue.size() - 1; i++) {
+      combineTimeRange(occupiedQueue, i);
     }
-    // create events to fill in the gap
-    TimeRange vacantSlot = null;
-    for (int currentEventIdx = 0;
-         currentEventIdx < occupiedQueue.size() - 1;
-         currentEventIdx++) {
-      // |OE1|   <vacantEvent here>   |OE2|
-      TimeRange occupiedSlot1 = occupiedQueue.get(currentEventIdx);
-      TimeRange occupiedSlot2 = occupiedQueue.get(currentEventIdx + 1);
-      vacantSlot = TimeRange.fromStartEnd(
-        occupiedSlot1.end(), occupiedSlot2.start(), false);
-      vacantSlots.add(vacantSlot);
+  
+    // Create vacant timeslots according to gaps in unavailable timeslots
+    for (int i = 0; i < occupiedQueue.size() - 1; i++) {
+      vacantSlots.add(createVacantSlot(occupiedQueue, i));
     }
 
-    Iterator<TimeRange> vacantSlotsIter = vacantSlots.iterator();
-    // Filter based on duration
-    vacantSlots.removeIf(timeSlot -> isSlotInsufficient(timeSlot, request));
-    return (Collection<TimeRange>)vacantSlots;
+    // Filter out vacant slots that do not meet duration requirement
+    filterVacantSlots(vacantSlots, request);
+    return vacantSlots;
   }
 }
